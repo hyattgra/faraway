dc62c2b71ee9cd090818db745a286b59d90c7077
hyattgra <hyattgra@gmail.com>
2016-05-20 13:28:58 -0400

    Update Solution.java

--- 4Sum/Solution.java
+++ 4Sum/Solution.java
@@ -2,9 +2,7 @@
         List<List<Integer>> ret = new ArrayList<List<Integer>>();
         Arrays.sort(nums);
         for (int i = 0; i < nums.length - 3; i++) {
-
             for (int ii = i + 1; ii < nums.length - 2; ii++) {
-
                 for (int j = ii + 1, k = nums.length - 1; j < k; ) {
                     int tmp = nums[i] + nums[ii] + nums[j] + nums[k];
                     if (tmp == target) {
