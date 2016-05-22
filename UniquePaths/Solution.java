8dc29710203d30fe47298ebf38832bb2d0672ac1
hyattgra <hyattgra@gmail.com>
2016-05-22 15:33:55 -0400

    Update Solution.java

--- UniquePaths/Solution.java
+++ UniquePaths/Solution.java
@@ -2,8 +2,8 @@ public int uniquePaths(int m, int n) {
         int[] dp = new int[n];
         dp[0] = 1;
 
-        for (int i = 0; i < m + 1; i++) {
-            for (int j = 0; j < n + 1; j++) {
+        for (int i = 0; i < m ; i++) {
+            for (int j = 1; j < n ; j++) {
                 dp[j] = dp[j] + dp[j-1];
             }
         }
