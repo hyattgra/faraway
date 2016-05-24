8034e519d67e9fec4fe768a665ed2760545d62ba
hyattgra <hyattgra@gmail.com>
2016-05-23 21:29:14 -0400

    Update Solution.java

--- Permutations/Solution.java
+++ Permutations/Solution.java
@@ -1,23 +1,23 @@
 public static List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ret = new ArrayList<List<Integer>>();
-
-        helper(ret, new ArrayList<>(), nums);
-
+        boolean[] used = new boolean[nums.length];
+        helper(ret, new ArrayList<>(), nums, used);
         return ret;
     }
 
-    private static void helper(List<List<Integer>> ret, List<Integer> tmp, int[] nums) {
+    private static void helper(List<List<Integer>> ret, List<Integer> tmp, int[] nums, boolean[] used) {
         if (tmp.size() == nums.length) {
             ret.add(new ArrayList<>(tmp));
             return;
         }
 
         for (int i = 0; i < nums.length; i++) {
-            if (!tmp.contains(nums[i])) {
+            if (!used[i]) {
+                used[i] = true;
                 tmp.add(nums[i]);
-                helper(ret, tmp, nums);
+                helper(ret, tmp, nums, used);
                 tmp.remove(tmp.size() - 1);
+                used[i] = false;
             }
         }
-
     }
