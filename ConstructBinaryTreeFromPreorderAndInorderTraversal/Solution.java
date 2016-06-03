393921c0797934003f9622d59524d48cdb93bc97
hyattgra <hyattgra@gmail.com>
2016-06-03 16:56:23 -0400

    Create Solution.java

---
+++ ConstructBinaryTreeFromPreorderAndInorderTraversal/Solution.java
@@ -0,0 +1,23 @@
+public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder == null || inorder == null) {
+            return null;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i], i);
+        }
+        return build(preorder,0,preorder.length-1, inorder, 0, inorder.length-1,map);
+    }
+
+    private TreeNode build(int[] preorder, int preLeft, int preRight,
+                           int[] inorder, int inLeft, int inRight, Map<Integer, Integer> map) {
+        if (preLeft > preRight || inLeft > inRight) {
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[preLeft]);
+        int rootIndex = map.get(root.val);
+        root.left = build(preorder, preLeft + 1, preLeft + rootIndex - inLeft, inorder, inLeft, rootIndex - 1, map);
+        root.right = build(preorder, preLeft + rootIndex - inLeft + 1, preRight, inorder, rootIndex + 1, inRight, map);
+
+        return root;
+    }
